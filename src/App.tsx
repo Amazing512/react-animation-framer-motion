e137c468e3baf66e5d2074095e043a741429b1e6
Matheus Mastelini <matheusmastelini@gmail.com>
2022-05-13 11:07:01 -0300

    First Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { motion, useMotionValue, useTransform } from "framer-motion";
 
 function App() {
+  const x = useMotionValue(0);
+  const opacity = useTransform(x, [-200, 0, 200], [0, 1, 0]);
+
+  const [value, setValue] = useState(0);
+  const [toggle, setToggle] = useState(1);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <motion.div
+      className='App'
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5 }}
+    >
+      <header className='App-header'>
+        <motion.div
+          whileTap={{ scale: 0.9 }}
+          drag='x'
+          dragConstraints={{ left: -200, right: 200 }}
+          style={{ x, opacity }}
         >
-          Learn React
-        </a>
+          <img src={logo} className='App-logo' alt='logo' />
+        </motion.div>
+        <motion.p
+          animate={{ x: value + "px", opacity: toggle }}
+          drag='x'
+          onDrag={(event, info) => {
+            setValue(x.get());
+          }}
+          dragConstraints={{ left: -100, right: 100 }}
+          whileTap={{ scale: 0.9 }}
+          whileHover={{ scale: 1.1 }}
+        >
+          Edit <code>src/App.tsx</code> and save to reload.
+        </motion.p>
+
+        <input
+          type='range'
+          name='range'
+          min='-100'
+          max='100'
+          onChange={(e) => setValue(parseInt(e.target.value))}
+        />
+
+        <motion.button
+          className='toggle-button'
+          onClick={() => setToggle(toggle == 0 ? 1 : 0)}
+        >
+          Toggle Fade
+        </motion.button>
       </header>
-    </div>
+    </motion.div>
   );
 }
 
